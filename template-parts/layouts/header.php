5bcd905da9988d128ac73fd8cdea51f3fa52b5c9
hailm <hai.lemanh.1310@gmail.com>
2022-05-12 10:12:02 +0700

    init project

---
+++ template-parts/layouts/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>BASE - <?= /** @var TYPE_NAME  */
+        (isset($data['title'])) ? $data['title'] : ''; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <!--CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css?v=<?php echo(time()) ?>">
+    <!--JS-->
+    <script src="https://cdn.jsdelivr.net/npm/@shinsenter/defer.js/dist/defer_plus.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <script src="js/app.js?v=<?php echo(time()) ?>"></script>
+</head>
+<body class="<?= (isset($body))?$body:'' ?>">
+<?php require "template-parts/commons/facebook_chat.php"; ?>
+<!--app-->
+<div id="app" class="min-vh-100">
