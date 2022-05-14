45bf622a008dc06ab72d5799ac3c206a5921e826
hailm <hai.lemanh.1310@gmail.com>
2022-05-14 11:00:19 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
-
+<nav class="game-navbar">
+    <div class="game-navbar__left">
+        <div class="game-navbar__item">
+            <a href="" class="game-navbar__icon game-navbar__icon--cup">
+                <div></div>
+            </a>
+        </div>
+    </div>
+    <div class="game-navbar__right">
+        <div class="game-navbar__item">
+            <a href="" class="game-navbar__icon game-navbar__icon--setup">
+                <div></div>
+            </a>
+        </div>
+    </div>
+</nav>
+<div class="home__desktop1 min-vh-100 d-flex align-items-center text-center">
+    <div class="w-100 home__desktop1__section">
+        <div class="container">
+            <div class="item__68">
+                <div><img src="images/logo31.png" alt=""></div>
+                <div class="home__desktop1__box">
+                    <div class="home__desktop1__boxTag">#JomHalauDenggi</div>
+                </div>
+            </div>
+            <div class="item__68">
+                <div class="item__9">
+                    <div class="home__desktop1__txt">WELCOME JUSTIN</div>
+                </div>
+                <div class="item__9">
+                    <a href="" class="home__desktop1__btn">START</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
